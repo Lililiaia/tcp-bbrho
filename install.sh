15ebd7fd62b775c97bb4ee05bf71ccb3410c322f
zhaoxin <xionyuno@gmail.com>
2022-08-28 15:57:20 +0000

    update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash 
 
+if [ -f installed.ok ]; then
+    echo "nothing to do"
+    exit
+fi
+
 # check git tool
 if ! command -v git &> /dev/null; then
     echo "please install git"
@@ -11,13 +16,13 @@ EXAMPLES_DIR="ns3-mmwave/examples/tcp-chna/"
 
 if [ ! -d "ns3-mmwave/" ];then
     echo "Downloading ns-mmwave(5.0)..."
-    git clone $MMWAVE_REPOS_URL
+    git clone $MMWAVE_REPOS_URL || exit "$?"
 fi
 
 # copy examples into ns3-mmwave/examples/tcp-cnha/
 if [ ! -d $EXAMPLES_DIR ];then
     echo "Create tcp-chna directory.."
-    mkdir ${EXAMPLES_DIR}
+    mkdir ${EXAMPLES_DIR} || exit "$?"
 fi
 
 cp examples/* $EXAMPLES_DIR
@@ -26,14 +31,17 @@ cp examples/* $EXAMPLES_DIR
 
 # configurating waf and put a patch
 cd ns3-mmwave/
-git am ../0001-dpdk.patch
-./waf configure --disable-python --enable-examples && ./waf build
+git am ../0001-dpdk.patch || exit "can not put dpdk patch into mmwave"
+./waf configure --disable-python --enable-examples && ./waf build || exit "$?"
 cd ../
 
 # create a execute file named as waf
 if [ ! -f "waf" ]; then
-echo "#!/bin/bash 
+echo "#!/bin/bash
+cp examples/* ${EXAMPLES_DIR}
 cd ns3-mmwave
 ./waf \$*" > waf
 chmod +x waf
 fi
+
+touch installed.ok
